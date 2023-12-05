92acdcb7d3a027ed4677a72b0c9a7b1c75b1dd56
Karol Bielecki <bielecki.karol.private@gmail.com>
2023-12-05 15:55:21 +0100

    Aktualize updated_at during translation_keys import

--- app/jobs/rails_i18n_manager/translations_import_job.rb
+++ app/jobs/rails_i18n_manager/translations_import_job.rb
@@ -43,7 +43,7 @@ module RailsI18nManager
             if val_record.nil?
               translation_values_to_import << key_record.translation_values.new(locale: locale, translation: val)
             elsif val_record.translation.blank? || (overwrite_existing && val_record.translation != val)
-              val_record.update!(translation: val)
+              val_record.update!(translation: val, updated_at: Time.current)
               next
             end
           end
@@ -56,11 +56,11 @@ module RailsI18nManager
       if mark_inactive_translations
         app_record.translation_keys
           .where.not(key: all_keys)
-          .update_all(active: false)
+          .update_all(active: false, updated_at: Time.current)
 
         app_record.translation_keys
           .where(key: all_keys)
-          .update_all(active: true)
+          .update_all(active: true, updated_at: Time.current)
       end
 
       return true
